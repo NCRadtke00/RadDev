3f0047bf22f2da0579e73efaed61cfbabee4e1ea
RadtkeN <raddnc@gmail.com>
2022-05-13 01:17:32 -0500

    installing packages and setting up tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
 export default MyApp
