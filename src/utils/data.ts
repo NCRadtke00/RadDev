980616f769ab2c602fdffc6f60070ef8767baeb8
RadtkeN <raddnc@gmail.com>
2022-05-18 01:40:02 -0500

    setting up utilities for data that will be used in project, starting with languages and currency

---
+++ src/utils/data.ts
@@ -0,0 +1,21 @@
+export const languages = [
+    "English",
+    "Spanish",
+    "French",
+    "German",
+    "Italian",
+    "Tamil",
+    "Hindi",
+    "Chinese",
+    "Russian",
+    "Japanese",
+    "Arabic",
+];
+export const currency = [
+      "$ - USD", 
+      "€ - EUR", 
+      "£ - GBP",
+      "₹ - INR", 
+      "¥ - JPY",
+      "¥ - CNY"
+];
