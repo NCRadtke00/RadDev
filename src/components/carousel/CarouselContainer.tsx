bd91eb60e0ec4ec004b4cc637db70086aba402b6
RadtkeN <raddnc@gmail.com>
2022-05-24 00:53:47 -0500

    setting up carousel component need to map through images and configure carousel

--- src/components/carousel/CarouselContainer.tsx
+++ src/components/carousel/CarouselContainer.tsx
@@ -1,3 +1,19 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+const images = ["BackToSchoolSales.png", "March.png", "giveback.png", "summersale45.png", "freeshipping.png", "FallLine.png", "buyonegetone.png", "boatsboatsboats40.png","Birthday.png", "blackfridaysale.png"]
+interface CarouselContainerProps {}
+
+ const CarouselContainer: React.FC<CarouselContainerProps>= ({}) => {
+  return (
+    <Carousel
+
+    >
+     {/* images will go here, will map and call each index */}
+   
+    </Carousel>
+  )
+}
+
+export default CarouselContainer
+
