900687f20be8832b42a55f79700e4a5f5f66a5ca
RadtkeN <raddnc@gmail.com>
2022-05-23 23:31:44 -0500

    added animations for sliding and transitioning, took from framer-motion sandbox

---
+++ src/utilities/animations.ts
@@ -0,0 +1,18 @@
+export const pageSlide = {
+    initial:{
+        opacity:0,
+        y:"-1rem",
+    },
+    in:{
+        opacity:1,
+        y:0,
+    },
+    out: {
+        opacity:0,
+        y:"1rem",
+    },
+};
+export const pageTransition = {
+    type: "tween",
+    duration: 0.2,
+}
