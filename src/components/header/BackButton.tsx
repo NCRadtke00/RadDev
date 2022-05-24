4b33ac3bf7ed73e8299e87db906dc1dd5500cbbd
RadtkeN <raddnc@gmail.com>
2022-05-24 01:56:03 -0500

    added backbutton

---
+++ src/components/header/BackButton.tsx
@@ -0,0 +1,16 @@
+import {useRouter} from "next/router";
+
+interface Props {
+    
+}
+
+const BackButton = (props: Props) => {
+    return (
+        
+        <button className="">
+            back
+        </button>
+    )
+}
+
+export default BackButton
