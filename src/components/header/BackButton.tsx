f11cde0c809fe22c4841f485f05031d9e8546322
RadtkeN <raddnc@gmail.com>
2022-05-24 01:56:33 -0500

    added styling to backbutton

--- src/components/header/BackButton.tsx
+++ src/components/header/BackButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 const BackButton = (props: Props) => {
     return (
         
-        <button className="">
+        <button className="mr-4 sm:mr-auto py-[0.33rem] px-[0.66rem] bg-white shadow-md rounded-lg text-sm relative">
             back
         </button>
     )
