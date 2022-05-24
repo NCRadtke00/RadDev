91f7f185dc3a27ce672bc10f6830d224f60955d8
RadtkeN <raddnc@gmail.com>
2022-05-24 02:11:43 -0500

    outline for footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Footer = (props: Props) => {
+    return (
+        <div>
+            footer stuff here
+        </div>
+    )
+}
+
+export default Footer
