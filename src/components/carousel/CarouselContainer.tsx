15eb96d4f8293bbd6333a9806320d4ab31b3d8da
RadtkeN <raddnc@gmail.com>
2022-05-24 01:13:45 -0500

    should be importing next/image but having issues with sizing. leaving as img from html for time being

--- src/components/carousel/CarouselContainer.tsx
+++ src/components/carousel/CarouselContainer.tsx
@@ -1,6 +1,6 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+// import Image from 'next/image'
 const images = ["BackToSchoolSale.png", "March.png", "giveback.png", "summersale45.png", "freeshipping.png", "FallLine.png", "buyonegetone.png", "boatsboatsboats40.png","Birthday.png", "blackfridaysale.png"]
 interface CarouselContainerProps {}
 
@@ -16,10 +16,13 @@ interface CarouselContainerProps {}
     interval= {4500}
     transitionTime={200}
     >
-     
       {images.map((image, index) => (
-        <img key={index} src={`/images/slides/${image}`} alt="carousel"/>
+        <img key={index} src={`/images/slides/${image}`} alt="carousel" />
         ))}
+        {/* shouldnt be using img and should be using imported Image from next/image but its causing the banners which are all sized great already to get wacky
+         {images.map((image, index) => (
+        <Image key={index} src={`/images/slides/${image}`} alt="carousel" width="" height="" layout="auto" />
+        ))} */}
     </Carousel>
   )
 }
