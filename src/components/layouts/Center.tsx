fe0ff451dd7f26afadcbabbabf1ebd6e9f65d8cc
RadtkeN <raddnc@gmail.com>
2022-05-24 02:12:20 -0500

    working on center layout

---
+++ src/components/layouts/Center.tsx
@@ -0,0 +1,16 @@
+import {motion} from "framer-motion";
+import {pageSlide, pageTransition} from "../../utilities/animations";
+
+interface CenterLayoutProps {
+    
+}
+
+const CenterLayout: React.FC<CenterLayoutProps> = ({}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CenterLayout;
