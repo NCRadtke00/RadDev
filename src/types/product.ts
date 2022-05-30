84d28ae403921ec6824260401240f17efd5243d8
RadtkeN <raddnc@gmail.com>
2022-05-30 04:40:23 -0500

    set up what information will be a user and a product

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    discount: boolean;
+    quantity: number;
+    category: string;
+    image: string;
+    description: string;
+  }
+  
---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export interface UserDetails {
+    name: string;
+    photoURL: string;
+    email: string;
+    address: string;
+    state: string;
+    zip: string;
+    country: string;
+    phone: string;
+    birth: string;
+  }
+  
