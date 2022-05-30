ab502c32a2fea4f1cda7e2b09d063a05346c7672
RadtkeN <raddnc@gmail.com>
2022-05-30 04:32:21 -0500

    finished back button for header using useRouter with nextjs

--- src/components/header/BackButton.tsx
+++ src/components/header/BackButton.tsx
@@ -1,16 +1,19 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-interface Props {
-    
-}
+interface BackButtonProps {}
 
-const BackButton = (props: Props) => {
-    return (
-        
-        <button className="mr-4 sm:mr-auto py-[0.33rem] px-[0.66rem] bg-white shadow-md rounded-lg text-sm relative">
-            back
+const BackButton: React.FC<BackButtonProps> = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      {router.pathname !== "/" && (
+        <button  className="mr-4 sm:mr-auto py-[0.33rem] px-[0.66rem] bg-white shadow-md rounded-lg text-sm relative" onClick={() => router.back()}>
+          Back
         </button>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default BackButton
+export default BackButton;
