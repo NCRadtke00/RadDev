797ce9a1ef696a9b0d8490e07ca43ffcef0ecafb
RadtkeN <raddnc@gmail.com>
2022-05-30 15:27:46 -0500

    copied style and had to fix error and remove this. before the onclick

--- src/components/dropdown/DropDownItem.tsx
+++ src/components/dropdown/DropDownItem.tsx
@@ -8,7 +8,8 @@ interface DropDownItemProps {
 
 const DropDownItem: React.FC<DropDownItemProps> = ({onClick, item}) => {
     return (
-        <li onClick={this.onClick}>
+        <li onClick={onClick} className="transition-colors duration-200 py-2 pr-8 pl-4
+        whitespace-nowrap first-of-type:pt-3 last-of-type:pb-3 hover:bg-gray-100">
             {item}
         </li>
     )
