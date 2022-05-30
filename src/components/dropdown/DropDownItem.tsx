1a303e8d2c6f9023677a146650e741f1e91a6887
RadtkeN <raddnc@gmail.com>
2022-05-30 15:23:21 -0500

    working on the dropdowns that are in footer/header

---
+++ src/components/dropdown/DropDownItem.tsx
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from 'react"
+import React from 'react'
+
+interface DropDownItemProps {
+    
+}
+
+const DropDownItem: React.FC<DropDownItemProps> = (click, item) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DropDownItem
