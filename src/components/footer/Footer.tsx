4927e69a24e927915ee05e13e0c972ca504e93fa
RadtkeN <raddnc@gmail.com>
2022-05-30 02:24:31 -0500

    working on footer, its not set to bottom of page yet but this is general outline with styling minus font

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-
-interface Props {
+import Disclaimer from './Disclaimer'
+import Links from './Links'
+import FooterBottom from './FooterBottom'
+interface FooterProps {
     
 }
 
-const Footer = (props: Props) => {
+const Footer: React.FC<FooterProps> = () => {
     return (
-        <div>
-            footer stuff here
+        <div className="mt-auto bg-[#d8d8d8] flex flex-col w-full sm:pl-24 mx-auto">
+            {/* <Disclaimer />
+            <Links />
+            <FooterBottom /> */}
+            footer stuff hereeeeeeeeeeeee
         </div>
     )
 }
