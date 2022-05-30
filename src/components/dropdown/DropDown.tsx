992efca54b1fd5884133bc842cee9f3886fda26e
RadtkeN <raddnc@gmail.com>
2022-05-30 15:56:51 -0500

    working on the drop down somethings not right need to go back and check outsideclick

--- src/components/dropdown/DropDown.tsx
+++ src/components/dropdown/DropDown.tsx
@@ -1,22 +1,23 @@
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown"
 import {motion} from "framer-motion"
+import OutsideClick from '../layouts/OutsideClick'
 import {useState} from "react"
 import DropDownItem from "./DropDownItem"
 interface DropDownProps {
     className: string;
+    menu?: string;
     items: string[];
     defaultItem: string;
     
 }
 
-const DropDown: React.FC<DropDownProps>= ({className, items, defaultItem}) => {
+const DropDown: React.FC<DropDownProps>= ({className, items, defaultItem, menu}) => {
     const [active, setActive] = useState(false);
     const [selected, setSelected] = useState(defaultItem);
     return (
-        <>
-        {/* need to check if clicked in our out as well as set a menu */}
-            <span onClick={(e)=> {setActive((active) => !active);}}> {selected} <KeyboardArrowDown/></span> 
-        </>
+        <OutsideClick open={active} onClose={() => setActive(false)}>
+            <span className={`py-[5px] pr-[2.5px] pl-[10px] bg-white shadow-md rounded-lg text-sm mr-4 flex items-center cursor-pointer relative ${className}`} onClick={(e)=> {setActive((active) => !active);}}> {selected} <KeyboardArrowDown/></span> 
+        </OutsideClick>
     )
 }
 
