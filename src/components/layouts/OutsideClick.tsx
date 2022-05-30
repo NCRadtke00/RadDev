62f6749c357833d54fb8321a6e4b0607ec175283
RadtkeN <raddnc@gmail.com>
2022-05-30 15:56:10 -0500

    working on detecting click

--- src/components/layouts/OutsideClick.tsx
+++ src/components/layouts/OutsideClick.tsx
@@ -5,10 +5,23 @@ interface OutsideClickProps {
     onClose: ()=>void;
 }
 
-const OutsideClick: React.FC<OutsideClickProps> = ({}) => {
+const OutsideClick: React.FC<OutsideClickProps> = ({opened, onClose, children}) => {
+    const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const checkIfOutsideClick = (e:any) =>{
+            if(opened && ref.current && !ref.current.contains(e.target)){
+                onClose();
+            }
+        };
+        document.addEventListener('mousedown', checkIfOutsideClick);
+        return () => {
+            document.removeEventListener('mousedown', checkIfOutsideClick);
+        }
+    }, [opened, onClose]);
+
     return (
-        <div>
-            if clicked inside of outside
+        <div ref={ref}>
+            {children}
         </div>
     )
 }
