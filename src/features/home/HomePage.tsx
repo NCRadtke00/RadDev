316f77562434c7531b4c5d9bb614bf381b518f6d
RadtkeN <raddnc@gmail.com>
2022-05-30 18:30:09 -0500

    getting sidebar to appear so i can begin styling it, i want it on the right side of the page, ideally id give user option to select.

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -4,7 +4,7 @@ import CarouselContainer from "../../components/carousel/CarouselContainer";
 import ProductCategories from "../../features/product/feed/ProductCategories"
 import ProductMain from "../product/feed/ProductFeed";
 import Header from "../../components/header/Header";
-//import Sidebar from "../../components/sidebar/Sidebar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import Footer from "../../components/footer/Footer";
 
 interface HomePageProps {}
@@ -18,9 +18,9 @@ const HomePage: React.FC<HomePageProps> = () => {
             variants={pageSlide}
             transition={pageTransition}
         >
-            {/* <Sidebar/> */}
+           
             <Header/>
-            <CarouselContainer />
+            <CarouselContainer /> <Sidebar />
             <ProductCategories />
             <ProductMain />
             <Footer/>
