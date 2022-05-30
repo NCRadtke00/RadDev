95311e172be4f1bfeaf2d403e3b05e5985ce57a2
RadtkeN <raddnc@gmail.com>
2022-05-30 18:14:55 -0500

    setup a store to house all the substores, like the commonstore for loading and sidebar so users can open or close the sidebar for more screen viewing space.

---
+++ src/stores/store.ts
@@ -0,0 +1,16 @@
+import {createContext, useContext } from "react";
+import CommonStore from "./commonStore";
+
+
+interface Store {
+    commonStore: CommonStore;
+}
+
+export const store: Store = {
+  commonStore: new CommonStore(),
+};
+
+export const StoreContext = createContext(store);
+export const useStore = () => {
+    return useContext(StoreContext);
+};
