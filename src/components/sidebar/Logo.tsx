73d850f4ae41f463cefde3c5c835fb131c7d1440
RadtkeN <raddnc@gmail.com>
2022-05-30 20:41:58 -0500

    working on getting logo to slide sidebar. work in progress

---
+++ src/components/sidebar/Logo.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../stores/store";
+//import Image from 'next/image'
+interface LogoProps {
+    
+}
+
+const Logo: React.FC<LogoProps> = () => {
+    const {sidebarActive, toggleSidebar} = useStore().commonStore;
+
+    return (
+        <img
+            loading="lazy"
+            src="/images/icons/hoodie.svg"
+            alt="icon"
+            onClick={toggleSidebar}
+            className={`w-10 sm:w-8 z-20 transition-all duration-200
+            transform translate-x-20 sm:translate-x-0 p-2 sm:p-0
+            rounded-full bg-white shadow-md sm:shadow-none ${
+              sidebarActive && "!translate-x-0 !shadow-none"
+            }`}
+        />
+    )
+}
+
+export default observer(Logo)
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 import {observer} from 'mobx-react-lite';
 import dynamic from "next/dynamic";
 import {useStore} from "../../stores/store"
+import Logo from "./Logo"
+
+const ReactTooltip = dynamic(() => import("react-tooltip"), { ssr: false });
 
 
 interface SidebarProps {}
@@ -9,8 +12,18 @@ const Sidebar: React.FC<SidebarProps> = () => {
     const {sidebarActive} = useStore().commonStore;
 
     return (
-        <div >
-            side barrr
+        <div  className={`wr-20 sm:w-24 h-screen fixed z-50 top-0 right-0
+        shadow-lg bg-white flex flex-col justify-between
+        items-center py-6 sm:py-12 transition-all duration-200
+        transform translate-x-20 sm:translate-x-0
+        ${sidebarActive && "translate-x-0"}`}>
+            <Logo />
+            <ReactTooltip
+                place="left"
+                id="sidebarToolTip"
+                backgroundColor="#201a2ced"
+                effect="solid"
+                className="!rounded-lg whitespace-nowrap" />
         </div>
     )
 }
