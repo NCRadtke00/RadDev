fde88054468a872454b6bf0bea88f8fcb3133393
RadtkeN <raddnc@gmail.com>
2022-05-31 00:28:49 -0500

    working on applayout

---
+++ src/components/layouts/AppLayout.tsx
@@ -0,0 +1,17 @@
+import {observer} from "mobx-react-lite"
+import {useEffect, useRef} from "react"
+import {useStore} from "../../stores/store"
+
+interface AppLayoutProps {
+    
+}
+
+const AppLayout: React.FC<AppLayoutProps> = ({children}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AppLayout
