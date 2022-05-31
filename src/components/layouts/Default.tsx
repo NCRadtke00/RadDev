01d1e624780b749d6f02b855fce814c1b4ba0c3e
RadtkeN <raddnc@gmail.com>
2022-05-31 00:25:25 -0500

    created the default layout

---
+++ src/components/layouts/Default.tsx
@@ -0,0 +1,28 @@
+import {AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import SideBar from "../sidebar/Sidebar"
+import Header from "../header/Header"
+import Footer from "../footer/Footer"
+interface DefaultLayoutProps {
+    
+}
+
+const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
+    return (
+        <div className="w-screen min-h-screen relative bg-[#fafafa] flex flex-col">
+           <SideBar />
+           <div className="sm:pl-24 w-full max-w-screen-1xl mx-auto flex-grow flex flex-col">
+            <AnimatePresence exitBeforeEnter>
+                <AnimateSharedLayout>
+                    <Header />
+                    {children}
+                </AnimateSharedLayout>
+                </AnimatePresence>
+            </div> 
+            <div className="mt-auto mb-20">
+                <Footer />
+            </div>
+        </div>
+    )
+}
+
+export default DefaultLayout;
