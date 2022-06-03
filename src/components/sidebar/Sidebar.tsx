07dcf585763c2a7a4a63fcf97107d870a3f3833f
RadtkeN <raddnc@gmail.com>
2022-06-02 23:41:26 -0500

    SIDEBAR IS WORKING Correctly!!!!

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -12,11 +12,7 @@ const Sidebar: React.FC<SidebarProps> = () => {
     const {sidebarActive} = useStore().commonStore;
 
     return (
-        <div  className={`wr-20 sm:w-24 h-screen fixed z-50 top-0 right-0
-        shadow-lg bg-white flex flex-col justify-between
-        items-center py-6 sm:py-12 transition-all duration-200
-        transform translate-x-20 sm:translate-x-0
-        ${sidebarActive && "translate-x-0"}`}>
+        <div  className={`wr-20 sm:w-24 h-screen fixed z-50 top-0 right-0 shadow-lg bg-white flex flex-col justify-between items-center py-6 sm:py-12 transition-all duration-200 transform translate-x-20 sm:translate-x-[0px] ${sidebarActive && "translate-x-0"}`}>
             <Logo />
             <ReactTooltip
                 place="left"
