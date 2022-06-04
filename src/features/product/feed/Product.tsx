1a216fdd77718ab138d417f2ef9e29b213704888
RadtkeN <raddnc@gmail.com>
2022-06-04 01:12:16 -0500

    set up simple product card, going to make info and image files

--- src/features/product/feed/Product.tsx
+++ src/features/product/feed/Product.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { pageTransition, pageZoom } from "../../../utilities/animations";
+import { Product as ProductType } from '../../../types/product'
+import { observer } from "mobx-react-lite";
 
-interface Props {
-    
+
+interface ProductProps {
+    product: ProductType;
 }
 
-const Product = (props: Props) => {
+const Product: React.FC<ProductProps> = ({ product }) => {
+
     return (
-        <div>
-            
-        </div>
+        <motion.div 
+            initial="initial"
+            animate="in"
+            exit="out"
+            transition={pageTransition}
+            variants={pageZoom}
+            layout
+            className="w-3/4 bg-white rounded0lg shadow-md flex flex-col transition-all duration-200 transform hover:scale-110 hover:drop-shadow-lg"
+        >
+            image 
+            info
+        </motion.div>
     )
 }
 
-export default Product
+export default observer(Product);
