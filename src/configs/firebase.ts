607e3e3ab42b0a26e5d7156207972d7bae375f42
RadtkeN <raddnc@gmail.com>
2022-06-05 00:18:59 -0500

    mostly copying doc from googleFirebase to set up

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,3 +1,11 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+
+const firebaseConfig = {
+
+}
+const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export (db, auth);
