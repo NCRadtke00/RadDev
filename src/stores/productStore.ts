1a343477988623e15d16182bac217a90dc56676e
RadtkeN <raddnc@gmail.com>
2022-06-05 00:54:54 -0500

    working on file to store crud operations of products

---
+++ src/stores/productStore.ts
@@ -0,0 +1,23 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import { Product } from "../types/product";
+import { store } from "./store";
+
+
+
+class ProductStore {
+    productRegistery = new Map<number, Product>();
+    selectedProduct? : Product;
+    activeCategory = "all products";
+    constructor(){
+        makeAutoObservable(this);
+    }
+    get products() {
+        return Array.from(this.productRegistery.values());
+    }
+    get totalProducts(){
+        return Array.from(this.productRegistery.values()).reduce(
+            (total, product) => product.quantity + total, 0);
+    }
+}
+
+export default ProductStore;
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,16 @@
 import { createContext, useContext } from "react";
 import CommonStore from "./commonStore";
+import ProductStore from "./productStore";
 
 
 interface Store {
     commonStore: CommonStore;
+    productStore: ProductStore;
 }
 
 export const store: Store = {
     commonStore: new CommonStore(),
+    productStore: new ProductStore(),
 };
 
 export const StoreContext = createContext(store);
