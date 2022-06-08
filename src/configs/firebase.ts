aedb371ef47fc937b38cf359245448c54436e90b
RadtkeN <raddnc@gmail.com>
2022-06-07 23:16:48 -0500

    setting up firebase/hosting/storage

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,8 +1,9 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
+
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
