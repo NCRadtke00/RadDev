a92dbaade33fcddf7196e3fb97c2cd849f5ebc00
RadtkeN <raddnc@gmail.com>
2022-06-08 00:02:49 -0500

    product page will have a large image of product, going to use next/image

---
+++ src/features/product/details/ProductCover.tsx
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../../stores/store";
+import Image from "next/image";
+
+interface ProductCoverProps {
+}
+
+const ProductCover: React.FC<ProductCoverProps> = () => {
+
+return (
+    <div></div>
+)
+}
+
+export default observer(ProductCover);
