90cfd4a1ca5845bf917af9ec17cd603fe2277bc8
RadtkeN <raddnc@gmail.com>
2022-06-09 00:23:04 -0500

    importing auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import '../styles/globals.css'
 import AppLayout from '../components/layouts/AppLayout';
 import DefaultLayout from '../components/layouts/Default';
 import { AppProps } from 'next/app';
-
+import { SessionProvider as AuthProvider } from 'next-auth/react'
 import { store, StoreContext } from "../stores/store"
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>  
+    <AuthProvider session={pageProps.session}>  
       <StoreContext.Provider value={store}>
           <DefaultLayout>
             <AppLayout>
@@ -15,7 +15,7 @@ const App = ({ Component, pageProps }: AppProps) => {
             </AppLayout>
           </DefaultLayout>
         </StoreContext.Provider>
-    </>
+    </AuthProvider>
   );
 };
 
