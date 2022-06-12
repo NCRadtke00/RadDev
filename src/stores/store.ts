5e5753bbfcbbb3a9cdf212f0f19fa8d23246296e
RadtkeN <raddnc@gmail.com>
2022-06-12 01:39:42 -0500

    needed to add bookmarks to the store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -3,13 +3,14 @@ import CommonStore from "./commonStore";
 import ProductStore from "./productStore";
 import SearchStore from "./searchStore";
 import CartStore from "./cartStore";
-
+import BookmarkStore from "./bookmarkStore";
 
 interface Store {
     commonStore: CommonStore;
     productStore: ProductStore;
     searchStore: SearchStore;
     cartStore: CartStore;
+    bookmarkStore: BookmarkStore;
 
 }
 
@@ -18,7 +19,7 @@ export const store: Store = {
     productStore: new ProductStore(),
     searchStore: new SearchStore(),
     cartStore: new CartStore(),
-
+    bookmarkStore: new BookmarkStore(),
 };
 
 export const StoreContext = createContext(store);
