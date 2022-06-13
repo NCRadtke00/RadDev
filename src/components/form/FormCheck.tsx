696501aec30af2aa3a6b153c4f4b4a4236e8bcf8
RadtkeN <raddnc@gmail.com>
2022-06-13 01:11:03 -0500

    finished component for checks in form

---
+++ src/components/form/FormCheck.tsx
@@ -0,0 +1,24 @@
+import styles from 'styles/form.module.css';
+
+interface FormCheckProps {
+    name: string;
+    label: string;
+    type: "checkbox" | "radio";
+    value: boolean;
+    onClick: () => void;
+}
+
+const FormCheck: React.FC<FormCheckProps> = ({
+    name, label,type, value, onClick,
+}) => {
+    return (
+        <div className={`relative mt-4 flex ${styles.check}`} onClick={onClick}>
+            <input type={type} name={name} checked={value} readOnly />
+            <label htmlFor={name} className="text-base">
+                {label}
+            </label>
+        </div>
+    )
+}
+
+export default FormCheck
