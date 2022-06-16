9ffbe6cef9ed6ae951b62a596ca301d662f846b0
RadtkeN <raddnc@gmail.com>
2022-06-16 01:51:43 -0500

    finished index page to stop errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import HomePage from "../features/home/HomePage";
 import Head from 'next/head'
 import { observer } from "mobx-react-lite";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { useStore } from "stores/store";
+import { useEffect } from "react";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  const { totalProducts, loadProducts } = useStore().productStore;
+  useEffect(() => {
+    if(totalProducts == 0 ) {
+      loadProducts();
+    }
+  }, [totalProducts, loadProducts])
  return (
    <>
    <Head>
@@ -17,3 +27,12 @@ const Home: React.FC<HomeProps> = () => {
 
 export default observer(Home);
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
